0882f72806c1102722a5b049a6f0c88e5b41626d
jstevenfm <jstevenfm@gmail.com>
2023-10-07 00:35:03 -0500

    feat - Added principal page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bgred: '#590007',
+			},
+			fontFamily: {
+				'roboto': ['"Roboto"', 'sans-serif;']
+			},
+		},
 	},
 	plugins: [],
 }
